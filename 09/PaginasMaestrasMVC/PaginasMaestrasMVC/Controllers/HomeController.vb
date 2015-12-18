b0b8faf59578d66f9d44a83c6a47739184a892b2
Francisco Charte Ojeda <francisco@fcharte.com>
2015-12-18 09:55:28 +0100

    Adds application controller

---
+++ 09/PaginasMaestrasMVC/PaginasMaestrasMVC/Controllers/HomeController.vb
@@ -0,0 +1,16 @@
+﻿Public Class HomeController
+    Inherits System.Web.Mvc.Controller
+
+    '
+    ' GET: /Home
+
+    Function Index() As ActionResult
+        Return View()
+    End Function
+
+    Function Novedades(id As String) As ActionResult
+        ViewData("titulo") = id
+        Return View()
+    End Function
+
+End Class

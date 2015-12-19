e02a7bc939593d54fb01ae8a6f0255e173278b87
Francisco Charte Ojeda <francisco@fcharte.com>
2015-12-19 20:12:03 +0100

    Adds PerfilesUsuario project

---
+++ 11/PerfilesUsuario/Default.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Default.aspx.vb" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+      <h1 style="text-align: center">
+        Perfiles ASP.NET 4.5</h1>
+    
+    </div>
+    <br />
+    <span class="style1">Lista de artículos disponibles</span><asp:CheckBoxList 
+      ID="ListaArticulos" runat="server" AutoPostBack="True" BackColor="#FFFF99" 
+      BorderStyle="Dotted" CellPadding="1" CellSpacing="1" Font-Names="Arial" 
+      ForeColor="#0033CC">
+      <asp:ListItem>Gestión de perfiles en ASP.NET 4.5</asp:ListItem>
+      <asp:ListItem>Registro y administración de usuarios</asp:ListItem>
+      <asp:ListItem>Sitios Web personalizables por el usuario</asp:ListItem>
+      <asp:ListItem>Introducción a ADO.NET</asp:ListItem>
+      <asp:ListItem>Enlace a datos en aplicaciones que usan formularios Web</asp:ListItem>
+      <asp:ListItem>Enlace a datos en aplicaciones basadas en MVC</asp:ListItem>
+      <asp:ListItem>ASP.NET y AJAX</asp:ListItem>
+    </asp:CheckBoxList>
+    <br />
+    <asp:Panel ID="Panel1" runat="server" BackColor="#FFFF99" 
+      GroupingText="Datos personales" Height="162px">
+      ID de inicio de sesión
+      <asp:TextBox ID="IDInicioSesion" runat="server" Enabled="False" Width="202px"></asp:TextBox>
+      <br />
+      <br />
+      Nombre
+      <asp:TextBox ID="Nombre" runat="server" Width="187px"></asp:TextBox>
+      <br />
+      <br />
+      Apellidos
+      <asp:TextBox ID="Apellidos" runat="server" Width="179px"></asp:TextBox>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <asp:Button ID="Button1" runat="server" Text="Actualizar perfil" />
+      <br />
+    </asp:Panel>
+    </form>
+</body>
+</html>
---
+++ 11/PerfilesUsuario/Default.aspx.vb
@@ -0,0 +1,56 @@
+﻿
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        ' Si no es la solicitud inicial no continuar
+        If IsPostBack Then Return
+
+        ' Para los usuarios que han iniciado sesión
+        If Not Profile.IsAnonymous Then
+            ' mostrar los datos personales
+            IDInicioSesion.Text = User.Identity.Name
+            Nombre.Text = Profile.Personales.Nombre
+            Apellidos.Text = Profile.Personales.Apellidos
+        Else
+            ' Si es un usuario anónimo, desactivar los
+            ' datos personales
+            Nombre.Enabled = False
+            Apellidos.Enabled = False
+            Button1.Enabled = False
+        End If
+
+        ' Mostrar los artículos ya leídos
+        For Each Articulo In Profile.Articulos
+            For Each Elemento As ListItem In ListaArticulos.Items
+                If Elemento.Value = Articulo Then
+                    Elemento.Selected = True
+                End If
+            Next
+        Next
+    End Sub
+
+    Protected Sub ListaArticulos_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListaArticulos.SelectedIndexChanged
+        ' Recorrer la lista de artículos
+        For Each Elemento As ListItem In ListaArticulos.Items
+            ' Añadiendo los marcados siempre que no estén
+            ' ya en la lista
+            If Elemento.Selected Then
+                If Profile.Articulos.IndexOf(Elemento.Value) =
+                             -1 Then
+                    Profile.Articulos.Add(Elemento.Value)
+                End If
+            Else ' y eliminando los que se hubiesen desmarcado
+                Profile.Articulos.Remove(Elemento.Value)
+            End If
+        Next
+
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Conservar los datos personales de usuarios identificados
+        Profile.Personales.Nombre = Nombre.Text
+        Profile.Personales.Apellidos = Apellidos.Text
+
+    End Sub
+End Class

85a019b7d7577c9376933b7dc373ddb59bb3883f
Francisco Charte Ojeda <francisco@fcharte.com>
2015-12-20 19:20:23 +0100

    Adds VistasParciales controller

---
+++ 13/VistasParciales/VistasParciales/Controllers/HomeController.vb
@@ -0,0 +1,23 @@
+﻿Public Class HomeController
+    Inherits System.Web.Mvc.Controller
+
+    '
+    ' GET: /Home
+
+    Function Index() As ActionResult
+        Return View()
+    End Function
+
+    <ChildActionOnly>
+    Function WebPart(id As String)
+        Select Case id
+            Case "Tipo1"
+                Return PartialView("Calendario")
+            Case "Tipo2"
+                Return PartialView("Enlaces")
+            Case Else
+                Return PartialView("Vacio")
+        End Select
+    End Function
+
+End Class

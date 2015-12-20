ab6301c4c5bcd3fa4c10a7c7ff05595e2c8797be
Francisco Charte Ojeda <francisco@fcharte.com>
2015-12-20 19:20:46 +0100

    Adds EntityFramework project

---
+++ 14/EntityFramework/Default.aspx.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Data.Objects
+Imports System.Data.Objects.DataClasses
+
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        ' Se obtiene el contexto de acceso a las entidades
+        Dim contextoBibilioteca = New BibliotecaEntities()
+
+        ' Se ejecuta una consulta para obtener los libros de un autor
+        Dim consultaLibros = contextoBibilioteca.libros.Where(
+           Function(libro) libro.autor = "Charte, Francisco")
+
+        Response.Write("<h1>Hay " & _
+         consultaLibros.Count &
+         " filas</h1><br />")
+        ' Configuración de la tabla
+        Response.Write("<table border='2' cellpadding='5'" &
+         " style='background-color: cyan; color: black; " &
+         "font-family: Arial; font-weight: bold'>")
+
+
+        ' Se enumeran los resultados
+        For Each Fila In consultaLibros
+            Response.Write("<tr><td>" & Fila.titulo & "</td></tr>")
+        Next
+
+        Response.Write("</table>")
+    End Sub
+End Class

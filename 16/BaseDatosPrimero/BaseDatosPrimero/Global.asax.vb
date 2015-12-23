7a36149e51dd084fe5a9790987fb51388a861542
Francisco Charte Ojeda <francisco@fcharte.com>
2015-12-23 18:27:26 +0100

    BaseDatosPrimero project global configuration

---
+++ 16/BaseDatosPrimero/BaseDatosPrimero/Global.asax.vb
@@ -0,0 +1,15 @@
+﻿' Nota: para obtener instrucciones sobre cómo habilitar el modo clásico de IIS6 o IIS7, 
+' visite http://go.microsoft.com/?LinkId=9394802
+Imports System.Web.Http
+
+Public Class MvcApplication
+    Inherits System.Web.HttpApplication
+
+    Sub Application_Start()
+        AreaRegistration.RegisterAllAreas()
+
+        WebApiConfig.Register(GlobalConfiguration.Configuration)
+        FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters)
+        RouteConfig.RegisterRoutes(RouteTable.Routes)
+    End Sub
+End Class

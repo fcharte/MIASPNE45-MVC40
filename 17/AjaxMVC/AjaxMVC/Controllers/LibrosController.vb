fbdb8a1e24505d8d6ec5b2b91cbd34fd8814a312
Francisco Charte Ojeda <francisco@fcharte.com>
2015-12-24 18:07:35 +0100

    AjaxMVC project controllers

---
+++ 17/AjaxMVC/AjaxMVC/Controllers/LibrosController.vb
@@ -0,0 +1,16 @@
+﻿Public Class LibrosController
+    Inherits System.Web.Mvc.Controller
+
+    Private db As New BibliotecaEntities
+    '
+    ' GET: /Libros
+
+    Function Index() As ActionResult
+        Return View(db.libros.ToList)
+    End Function
+
+    Function Detalles(Id As Integer)
+        Return PartialView(db.libros.Find(Id))
+    End Function
+
+End Class
